6dc567e4e048262a66cd42a64e4a80bc6f5483a6
Karol Dowbecki <karol@dowbecki.com>
2022-12-15 20:33:21 +0000

    Compressing html

--- run.sh
+++ run.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-yell() { echo "$0: $*" >&2; }
-die() { yell "$*"; exit 42; }
-try() { "$@" || die "Failed to: $*"; }
-
-try bundle exec jekyll serve
+bundle exec jekyll serve --incremental
